04475cdced8b989f053f16d539edeb5b3992c1cf
Tony O'Donnell <tonyod@gmail.com>
2025-12-15 17:05:40 +0000

    first commit

---
+++ src/webparts/sharepointReactWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISharepointReactWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SharepointReactWpWebPartStrings' {
+  const strings: ISharepointReactWpWebPartStrings;
+  export = strings;
+}
